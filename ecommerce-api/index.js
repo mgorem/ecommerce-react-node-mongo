6aa63f3a6322fa469ebe59309da313c7179b7704
mgorem <oremgitonga22@gmail.com>
2022-04-09 00:29:20 +0300

    client

--- ecommerce-api/index.js
+++ ecommerce-api/index.js
@@ -1,14 +1,14 @@
-const express = require ("express"); //importing the xpress library 
-const app = express(); //Configuriing the express app to use it
-const mongoose = require ("mongoose"); //importing the mondooselibrary 
+const express = require ("express"); //importing the express library 
+const app = express(); //Configuring the express app to use it
+const mongoose = require ("mongoose"); //importing the mongoose library 
 const dotenv = require ("dotenv"); //importing the dotenv library 
 
 dotenv.config(); //Configuring the dotenv library
 
-//Connectiong to the mongoDB Database
+//Connecting to the mongoDB Database
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("DB Connection Successful!"))
-.catch((err) => {console.log("err");});
+.catch((err) => {console.log(err);});
 
 //Using express app to ensure sever is running
 app.listen(process.env.PORT || 5000, () => {
